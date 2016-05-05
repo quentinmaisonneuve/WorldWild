20cf4053ca2caad35663ebff077ac5e2639c9295
Benjamin <b.delcluze@laposte.net>
2016-05-05 03:59:39 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test
     {
         Individu i = new Individu(15,18, 100,100,70,"Gérard","Dupont",Sexe.FEMME);
 
-        i.setAge(99);//Benjamin
+        i.setAge(45);//Benjamintest
         System.out.println(i.getBarreVieMax());
     }
 }
