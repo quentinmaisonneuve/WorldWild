50b1cbf27784059bfae4991ded5d18bad152477a
Quentin MAISONNEUVE <quentin.maisonneuve1@etu.univ-nantes.fr>
2016-05-05 03:09:04 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test
     {
         Individu i = new Individu(15,18, 100,100,70,"Gérard","Dupont",Sexe.FEMME);
 
-        i.setAge(99);//455
+        i.setAge(99);//Benjamin
         System.out.println(i.getBarreVieMax());
     }
 }
